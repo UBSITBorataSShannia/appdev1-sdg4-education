46c72f95244ad7883d2ae5856404c820220a5317
UBSITBorataSShannia <20225625@s.ubaguio.edu>
2026-04-20 10:13:03 +0800

    Week2 Commit 5: Fixed some Errors in the app.config.ts and Added withFetch in tthe code.

--- appdev1-sdg4-education/src/app/app.config.ts
+++ appdev1-sdg4-education/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,7 +7,7 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
     provideRouter(routes)
   ]
 };
